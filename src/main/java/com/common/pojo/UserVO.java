0b3078287e3466c4c4d602bdbe5bf75bfa02f4f4
zck <zck@qq.com>
2023-03-22 10:13:39 +0800

    test

--- src/main/java/com/common/pojo/UserVO.java
+++ src/main/java/com/common/pojo/UserVO.java
@@ -9,11 +9,10 @@ import java.time.LocalDateTime;
  * @author zck created in 2023/3/13 15:57
  */
 @Data
-public class User {
+public class UserVO {
     private String userName;
     private String password;
     private String phone;
-    private String email;
     private Integer userType;
     private LocalDateTime createTime;
 }
