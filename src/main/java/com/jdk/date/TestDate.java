e62fff8a93005df00291118d8c8b4d12315f9f25
zck <zck@qq.com>
2023-07-07 13:27:08 +0800

    test

--- src/main/java/com/jdk/date/TestDate.java
+++ src/main/java/com/jdk/date/TestDate.java
@@ -7,6 +7,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.time.temporal.TemporalAdjusters;
 import java.time.temporal.WeekFields;
 import java.util.*;
@@ -21,18 +22,15 @@ public class TestDate {
     }
 
 
+    /**
+     * 获取当前月是那个季度
+     * @throws InterruptedException
+     */
     @Test
     public void test_2023_03_14_15_31_10() throws InterruptedException {
-        User user = new User();
-        user.setCreateTime(LocalDateTime.now());
-        LocalDateTime createTime = user.getCreateTime();
-
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        System.out.println(createTime.format(formatter));
-        Thread.sleep(5000);
-//        LocalDateTime currentDateTime = LocalDateTime.now();
-//        String formattedDateTime = currentDateTime.format(formatter);
-        System.out.println(createTime.format(formatter));
+        LocalDateTime of = LocalDateTime.of(2023, 6, 20, 22, 22, 22);
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println(now.isAfter(of));
     }
 
     // 获取总周数
@@ -67,14 +65,22 @@ public class TestDate {
      */
     @Test
     public void test_demo_2023_06_25_09_56_07() {
-        // 获取当前日期
-        LocalDate currentDate = LocalDate.now();
-        // 获取一周的起始日期
-        WeekFields weekFields = WeekFields.of(Locale.getDefault());
-        // 计算当前时间是当年的第几周
-        int weekNumber = currentDate.get(weekFields.weekOfWeekBasedYear());
-        // 输出结果
-        System.out.println("当前时间是当年的第 " + weekNumber + " 周");
+//        // 获取当前日期
+//        // 获取 WeekFields
+//        WeekFields weekFields = WeekFields.of(Locale.getDefault()).withFirstDayOfWeek(DayOfWeek.MONDAY);
+//        // 计算当前时间是当年的第几周
+//        int weekNumber = currentDate.get(weekFields.weekOfWeekBasedYear());
+//        // 输出结果
+//        System.out.println("当前时间是当年的第 " + weekNumber + " 周");
+
+        LocalDate currentDate = LocalDate.of(2023,6,27);
+        LocalDate firstDayOfYear = LocalDate.of(currentDate.getYear(), 1, 1); // 创建表示一年的第一天的LocalDate对象
+        LocalDate firstDayOfWeek = firstDayOfYear.with(TemporalAdjusters.previousOrSame(WeekFields.ISO.getFirstDayOfWeek()));
+        System.out.println(firstDayOfWeek);
+        System.out.println(currentDate);
+        long between = ChronoUnit.DAYS.between(firstDayOfWeek, currentDate);
+        System.out.println(between / 7 + 1) ;
+
     }
 
 }
